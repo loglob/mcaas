144e6078df47e25cfb61311479887d214a252723
Loglob <l.gruem@web.de>
2020-02-07 15:48:12 +0100

    Improved HTML-Parsing mechanism
    Now using xmllint to make parsing more robust.

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # mcserver-download: Downloads the newest available minecraft server to minecraft_server.jar, overwriting any previous versions
 
-
-# Make sure we're in the server directory (or at least the directory the scripts are in)
-cd "$(realpath "$(dirname "$0")")"
+SRV_DIR=$(dirname "$(realpath "$0")")
+set -e
 
 if (! which curl > /dev/null 2>&1 )
 then
@@ -13,8 +12,14 @@ fi
 
 if (! which hxnormalize > /dev/null 2>&1 || ! which hxselect > /dev/null 2>&1 )
 then
-	echo "Missing hxnormalie and hxselect needed for parsing the Minecraft webpage."
+	echo "Missing hxnormalize and hxselect needed for parsing the Minecraft webpage."
 	echo "These are usually found in a html-xml-utils package."
+	exit
+fi
+
+if (! which xmllint > /dev/null 2>&1 )
+then
+	echo "Missing xmllint needed for parsing the Minecraft webpage."
 	exit
 fi
 
@@ -24,15 +29,16 @@ WEBPAGE="https://www.minecraft.net/en-us/download/server/"
 SELECTOR="a[href$=\"server.jar\"]"
 OUTFILE="minecraft_server.jar"
 
-DL_LINK=$(curl -s "$WEBPAGE" | hxnormalize -x | hxselect "$SELECTOR")
+# extract the <a> Element for the server.jar download
+LINK=$(wget -q -O - "$WEBPAGE" | hxnormalize -x | hxselect "$SELECTOR")
+# extract the Version of the found server.jar
+VERSION=$(echo "$LINK" | xmllint --xpath "string(/a)" -)
+# extract the URL of the found server.jar
+FILE=$(echo "$LINK" | xmllint --xpath "string(/a/@href)" -)
 
-DL_VERSION=$(echo "$DL_LINK" | cut -d"\"" -f 3 | cut -d">" -f 2 | cut -d"<" -f 1)
-DL_FILE=$(echo "$DL_LINK" | cut -d"=" -f 2 | cut -d"\"" -f 2)
+echo "Found $VERSION"
 
-echo "Found $DL_VERSION"
+wget -q --show-progress -O "$SRV_DIR/$OUTFILE" "$FILE"
+chmod +x "$SRV_DIR/$OUTFILE"
 
-wget -O "$OUTFILE" "$DL_FILE"
-chmod +x "$OUTFILE"
-mkdir -p bak world
-
-echo "eula=true" > eula.txt
+#echo "eula=true" > eula.txt
