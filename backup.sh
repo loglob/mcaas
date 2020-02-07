694f252b4b1505db6e22a56997c1488e6b0753c4
loglob <loglob@loglob.ddns.net>
2020-02-07 21:47:38 +0100

    Improved backup.sh If the server is running, save the world before making a backup

--- backup.sh
+++ backup.sh
@@ -4,6 +4,17 @@
 set -e
 dir="$(realpath "$(dirname "$0")")"
 timestamp="$(date "+%Y-%m-%d %H:%M:%S")"
+screen_session=$(cat "$dir/conf/screen_session")
+
+if ( screen -ls | grep -q -w -F "$screen_session" )
+then
+	# setup a process that listens for the server to be done saving
+	tail -f -n 0 latest.log | grep -q -F "[Server thread/INFO]: Saved the game" &
+	# flush world changes to disk
+	"$dir/exec.sh" save-all flush
+	# wait for flush to finish
+	wait $!
+fi
 
 mkdir -p "$dir/bak"
 jar cfM "$dir/bak/$timestamp.zip" -C "$dir" "world"
