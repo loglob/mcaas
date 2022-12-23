7d3e5d5383355c78647a5bf6798e065ad01b5dfa
loglob <23386237+loglob@users.noreply.github.com>
2022-12-23 03:20:41 +0100

    more reliable stop script  automatically detects service name

--- stop.sh
+++ stop.sh
@@ -2,9 +2,25 @@
 # stop.sh: Stops the server. Waits until server exits.
 set -e
 cd "$(realpath "$(dirname "$0")")"
+SERVICE=""
+
+if [ -e *.service ] 2>&-
+then
+	SERVICE=$(basename *.service .service)
+else
+	SERVICE="$(basename "$(pwd)")"
+	>&2 echo "Not sure how the service is called based on *.service files, going with '$SERVICE'"
+fi
 
 # retrieve PID of running server
-PID="$(systemctl show --property MainPID --value mc)"
+PID="$(systemctl show --property MainPID --value "$SERVICE")"
+
+if [ PID == 0 ]
+then
+	>&2 echo "Can't get PID for service $SERVICE, is it correct and running?"
+	exit 1
+fi
+
 # setup a background process that waits for the server to exit
 tail --pid=$PID -f /dev/null &
 
