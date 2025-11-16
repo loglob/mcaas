8e75ef5e96da43d7799dbba24545df1441bcb73d
loglob <23386237+loglob@users.noreply.github.com>
2025-11-16 09:04:29 +0100

    Name service after install directory

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ require () {
 		if (! which $i > /dev/null 2>&1 )
 		then
 			echo "Missing $i, which is required for $MSG"
-			exit
+			exit 1
 		fi
 	done
 }
@@ -55,7 +55,9 @@ mkdir -p "$dir"
 git clone --depth 1 https://github.com/loglob/mcaas "$dir"
 rm -rf "$dir/.git" "$dir/install.sh" "$dir/README.md"
 "$dir/update.sh"
-genService > "$dir/mc.service"
+service_name="$(basename "$dir").service"
+service="$dir/$service_name"
+genService > "$service"
 
 #run jar once to generate default server.properties etc
 (
@@ -67,29 +69,36 @@ genService > "$dir/mc.service"
 
 read -p "Do you accept the Minecraft EULA? (https://account.mojang.com/documents/minecraft_eula) " eula
 case $eula in
-	[Yy]* ) echo "eula=true" > "$dir/eula.txt"; echo "You can now start the server using '$dir/start.sh' or 'service mc start'.";;
-	* ) echo "You have to accept the EULA before you can use the server. You can do this by editing $dir/eula.txt";;
+	[Yy]* ) echo "eula=true" > "$dir/eula.txt"; echo "You can now start the server using '$dir/start.sh' or via the systemD service.";;
+	* ) echo "You have to accept the EULA before you can use the server. You can do this by editing '$dir/eula.txt'";;
 esac
 
 read -p "Do you want to install the service now? " serv
 case $serv in
 	[Yy]* )
-		ipath="/etc/systemd/system/mc.service"
-		sudo ln -sf "$(realpath "$dir/mc.service")" "$ipath"
-		systemctl daemon-reload
+		ipath="/etc/systemd/system/"
+
+		if [ ! -d "$ipath" ]
+		then
+			echo "Cannot install SystemD service because directory '$ipath' doesn't exist"
+			exit
+		fi
+
+		sudo ln -s "$service" "$ipath"
+		sudo systemctl daemon-reload
 		echo "link service file placed in $ipath"
 
 		read -p "Do you want to automatically start the server on system startup? " autoinstall
 		case $autoinstall in
 			[Yy]* )
-				systemctl enable mc
+				systemctl enable "$service_name"
 			;;
 			*)
-				echo "To enable manually, run 'systemctl enable mc'"
+				echo "To enable manually, run 'systemctl enable \"$service_name\"'"
 			;;
 		esac
 	;;
 	*)
-		echo "To install manually, copy or link '$dir/mc.service' to a systemd service directory."
+		echo "To install manually, copy or link '$service' to a systemd service directory."
 	;;
 esac
